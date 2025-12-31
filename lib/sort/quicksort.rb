0682c4d33a98f12a96030987392b39e9cbce83c9
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-30 21:30:11 -0500

    optimized quick sort randomized pivot to help prevent worst case time reduced overhead on small partitions by using insertion sort once a threshold is hit

--- lib/sort/quicksort.rb
+++ lib/sort/quicksort.rb
@@ -3,8 +3,15 @@
 require_relative 'sorted'
 
 class Quicksort
-  def self.quicksort(array, left_index, right_index)
+  THRESHOLD = 16
+
+  def self.quicksort(array, left_index = 0, right_index = array.length - 1)
     return if left_index >= right_index
+
+    pivot_index = rand(left_index..right_index)
+    temp = array[pivot_index]
+    array[pivot_index] = array[right_index]
+    array[right_index] = temp
 
     swap_index = left_index - 1
     pivot = array[right_index]
@@ -20,15 +27,29 @@ class Quicksort
     temp = array[swap_index]
     array[swap_index] = array[right_index]
     array[right_index] = temp
-    quicksort(array, left_index, swap_index - 1)
-    quicksort(array, swap_index + 1, right_index)
+    if (swap_index - 1) - left_index <= THRESHOLD
+      insertion_sort(array, left_index, swap_index - 1)
+    else
+      quicksort(array, left_index, swap_index - 1)
+    end
+    if right_index - (swap_index + 1) <= THRESHOLD
+      insertion_sort(array, swap_index + 1, right_index)
+    else
+      quicksort(array, swap_index + 1, right_index)
+    end
   end
 
-  def self.swap(array, left, right)
-    temp = array[left]
-    array[left] = array[right]
-    array[right] = temp
+  def self.insertion_sort(arr, left, right)
+    (left + 1..right).each do |i|
+      key = arr[i]
+      j = i - 1
+      while j >= left && arr[j] > key
+        arr[j + 1] = arr[j]
+        j -= 1
+      end
+      arr[j + 1] = key
+    end
   end
 
-  private_class_method :swap
+  private_class_method :insertion_sort
 end
